fe871dfd4e8c1f9ecf01467a6d13654677bf187f
nmcli <fake@example.com>
2025-03-21 10:58:51 +0900

    Commit on 2025-03-21 10:58:51

--- src/main/java/com/example/qwer/config/SecurityConfig.java
+++ src/main/java/com/example/qwer/config/SecurityConfig.java
@@ -2,20 +2,19 @@ package com.example.qwer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll() // 모든 요청 허용 (Hello World만 띄울 경우)
+            .authorizeHttpRequests((authz) -> authz
+                .anyRequest().permitAll()
             )
-            .csrf(csrf -> csrf.disable()); // CSRF 비활성화 (필요에 따라 설정)
-
+            .csrf().disable();
         return http.build();
     }
 }
